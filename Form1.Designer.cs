d6866edcd0b9034205a6bee7bbfcae18821b4661
anhhnvn <hangocanh2005vp@gmail.com>
2024-08-04 13:55:00 +0700

    init project

---
+++ Form1.Designer.cs
@@ -0,0 +1,346 @@
+﻿namespace BillWater
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtLoaiKh = new System.Windows.Forms.Label();
+            this.dbfhf = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtUserSd = new System.Windows.Forms.TextBox();
+            this.txtTmonth = new System.Windows.Forms.TextBox();
+            this.txtLmoth = new System.Windows.Forms.TextBox();
+            this.txtMember = new System.Windows.Forms.TextBox();
+            this.txtPhone = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.cbType = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.lsDanhSach = new System.Windows.Forms.ListView();
+            this.label9 = new System.Windows.Forms.Label();
+            this.txtTongTien = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(48, 62);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(20, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(48, 116);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(48, 162);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Phone";
+            // 
+            // txtLoaiKh
+            // 
+            this.txtLoaiKh.AutoSize = true;
+            this.txtLoaiKh.Location = new System.Drawing.Point(48, 209);
+            this.txtLoaiKh.Name = "txtLoaiKh";
+            this.txtLoaiKh.Size = new System.Drawing.Size(104, 16);
+            this.txtLoaiKh.TabIndex = 3;
+            this.txtLoaiKh.Text = "Type of cutomer";
+            // 
+            // dbfhf
+            // 
+            this.dbfhf.AutoSize = true;
+            this.dbfhf.Location = new System.Drawing.Point(48, 260);
+            this.dbfhf.Name = "dbfhf";
+            this.dbfhf.Size = new System.Drawing.Size(122, 16);
+            this.dbfhf.TabIndex = 4;
+            this.dbfhf.Text = "Number of member";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(621, 26);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(72, 16);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "This month";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(457, 26);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(71, 16);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Last month";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(457, 122);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(127, 16);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Amout of water used";
+            // 
+            // txtUserSd
+            // 
+            this.txtUserSd.Location = new System.Drawing.Point(460, 156);
+            this.txtUserSd.Name = "txtUserSd";
+            this.txtUserSd.ReadOnly = true;
+            this.txtUserSd.Size = new System.Drawing.Size(100, 22);
+            this.txtUserSd.TabIndex = 8;
+            // 
+            // txtTmonth
+            // 
+            this.txtTmonth.Location = new System.Drawing.Point(624, 62);
+            this.txtTmonth.Name = "txtTmonth";
+            this.txtTmonth.Size = new System.Drawing.Size(100, 22);
+            this.txtTmonth.TabIndex = 9;
+            // 
+            // txtLmoth
+            // 
+            this.txtLmoth.Location = new System.Drawing.Point(460, 62);
+            this.txtLmoth.Name = "txtLmoth";
+            this.txtLmoth.Size = new System.Drawing.Size(100, 22);
+            this.txtLmoth.TabIndex = 10;
+            // 
+            // txtMember
+            // 
+            this.txtMember.Location = new System.Drawing.Point(175, 257);
+            this.txtMember.Name = "txtMember";
+            this.txtMember.ReadOnly = true;
+            this.txtMember.Size = new System.Drawing.Size(100, 22);
+            this.txtMember.TabIndex = 11;
+            // 
+            // txtPhone
+            // 
+            this.txtPhone.Location = new System.Drawing.Point(175, 162);
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.Size = new System.Drawing.Size(100, 22);
+            this.txtPhone.TabIndex = 12;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(175, 110);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 22);
+            this.txtName.TabIndex = 13;
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(175, 62);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(100, 22);
+            this.txtID.TabIndex = 14;
+            // 
+            // cbType
+            // 
+            this.cbType.FormattingEnabled = true;
+            this.cbType.Items.AddRange(new object[] {
+            "Household customer",
+            "Administrative agency",
+            "Production units",
+            "Business services"});
+            this.cbType.Location = new System.Drawing.Point(175, 209);
+            this.cbType.Name = "cbType";
+            this.cbType.Size = new System.Drawing.Size(121, 24);
+            this.cbType.TabIndex = 15;
+            this.cbType.SelectedIndexChanged += new System.EventHandler(this.cbType_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(221, 386);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 16;
+            this.button1.Text = "Add";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(488, 386);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 17;
+            this.button2.Text = "Edit";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(707, 386);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 18;
+            this.button3.Text = "Delete";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(877, 386);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 19;
+            this.button4.Text = "Reset";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // lsDanhSach
+            // 
+            this.lsDanhSach.HideSelection = false;
+            this.lsDanhSach.Location = new System.Drawing.Point(12, 448);
+            this.lsDanhSach.Name = "lsDanhSach";
+            this.lsDanhSach.Size = new System.Drawing.Size(1222, 171);
+            this.lsDanhSach.TabIndex = 20;
+            this.lsDanhSach.UseCompatibleStateImageBehavior = false;
+            this.lsDanhSach.SelectedIndexChanged += new System.EventHandler(this.lsDanhSach_SelectedIndexChanged);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(460, 208);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(130, 16);
+            this.label9.TabIndex = 21;
+            this.label9.Text = "Total price (with Tax)";
+            // 
+            // txtTongTien
+            // 
+            this.txtTongTien.Location = new System.Drawing.Point(463, 254);
+            this.txtTongTien.Name = "txtTongTien";
+            this.txtTongTien.ReadOnly = true;
+            this.txtTongTien.Size = new System.Drawing.Size(100, 22);
+            this.txtTongTien.TabIndex = 22;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(855, 68);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 16);
+            this.label4.TabIndex = 23;
+            this.label4.Text = "label4";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(822, 110);
+            this.textBox1.MinimumSize = new System.Drawing.Size(4, 210);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(210, 22);
+            this.textBox1.TabIndex = 24;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1246, 663);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtTongTien);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.lsDanhSach);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.cbType);
+            this.Controls.Add(this.txtID);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.txtPhone);
+            this.Controls.Add(this.txtMember);
+            this.Controls.Add(this.txtLmoth);
+            this.Controls.Add(this.txtTmonth);
+            this.Controls.Add(this.txtUserSd);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.dbfhf);
+            this.Controls.Add(this.txtLoaiKh);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label txtLoaiKh;
+        private System.Windows.Forms.Label dbfhf;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtUserSd;
+        private System.Windows.Forms.TextBox txtTmonth;
+        private System.Windows.Forms.TextBox txtLmoth;
+        private System.Windows.Forms.TextBox txtMember;
+        private System.Windows.Forms.TextBox txtPhone;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.ComboBox cbType;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ListView lsDanhSach;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox txtTongTien;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Windows.Forms;
+
+namespace BillWater
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void lsDanhSach_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (cbType.SelectedIndex == 0)
+            {
+                dbfhf.Text = "1";
+            }
+
+            lsDanhSach.View = View.Details;
+            lsDanhSach.GridLines = true;
+            lsDanhSach.FullRowSelect = true;
+
+            //tạo ra các cột cho listView
+            lsDanhSach.Columns.Add("ID", 50);
+            lsDanhSach.Columns.Add("Name", 150);
+            lsDanhSach.Columns.Add("Phone", 100);
+            lsDanhSach.Columns.Add("Type of cutomer", 200);
+            lsDanhSach.Columns.Add("Number of member", 80);
+            lsDanhSach.Columns.Add("Water last month(m3)", 80);
+            lsDanhSach.Columns.Add("Water this moth(m3)", 80);
+            lsDanhSach.Columns.Add("Water user", 80);
+            lsDanhSach.Columns.Add("Total price (with Tax)", 80);
+            lsDanhSach.Columns.Add("Description", 200);
+            //Tạo ra một đối tượng có kiểu ListViewItem
+            ListViewItem std_1 = new ListViewItem();
+            std_1.Text = "1";
+            std_1.SubItems.Add("Anderson");
+            std_1.SubItems.Add("014399999");
+            std_1.SubItems.Add("Household customer");
+            std_1.SubItems.Add("1");
+            std_1.SubItems.Add("1");
+            std_1.SubItems.Add("1");
+            std_1.SubItems.Add("1");
+            std_1.SubItems.Add("1");
+            std_1.SubItems.Add("1");
+            lsDanhSach.Items.Add(std_1);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string ID = txtID.Text;
+            string Name = txtName.Text;
+            string Phone = txtPhone.Text;
+            string Type = null;
+            if (cbType.SelectedIndex == 0)
+            {
+                Type = "Household customer";
+            }
+            else if (cbType.SelectedIndex == 1)
+            {
+                Type = "Administrative agency";
+            }
+            else if (cbType.SelectedIndex == 2)
+            {
+                Type = "Production units";
+            }
+            else if (cbType.SelectedIndex == 3)
+            {
+                Type = "Business services";
+            }
+              string Member = txtMember.Text;
+            string Lmoth=txtLmoth.Text;
+            string Tmoth=txtTmonth.Text;
+            string UserSd=txtUserSd.Text;
+            string Tong=txtTongTien.Text;
+
+            string Description = textBox1.Text;
+            ListViewItem item = new ListViewItem();
+            item.Text = ID;
+            item.SubItems.Add(Name);
+            item.SubItems.Add(Phone);
+            item.SubItems.Add(Type);
+            item.SubItems.Add(Member);
+            item.SubItems.Add(Lmoth);
+            item.SubItems.Add(Tmoth);
+            item.SubItems.Add(UserSd);
+            item.SubItems.Add(Tong);
+            item.SubItems.Add(Description);
+            lsDanhSach.Items.Add(item);
+        }
+
+        private void cbType_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            if (cbType.SelectedIndex == 0)
+            {
+                if (txtTmonth.Text == "" || txtLmoth.Text == "")
+                {
+                    MessageBox.Show("Enter Last moth and This moth in full");
+                }
+                else
+                {
+                    float Lmoth = float.Parse(txtLmoth.Text);
+                    float Tmoth = float.Parse(txtTmonth.Text);
+                    if ((Tmoth - Lmoth) <= 10)
+                    {
+                        float Uer = Tmoth - Lmoth;
+                        txtMember.Text = "5.973";
+                        txtUserSd.Text = Uer.ToString();
+                        txtTongTien.Text = (float.Parse(txtMember.Text) * float.Parse(txtUserSd.Text) * 0.1).ToString("F2");
+                    }
+                    else if ((Tmoth - Lmoth) <= 20)
+                    {
+                        float Uer = Tmoth - Lmoth;
+                        txtMember.Text = "7.052";
+                        txtUserSd.Text = Uer.ToString();
+                        txtTongTien.Text = (float.Parse(txtMember.Text) * float.Parse(txtUserSd.Text) * 0.1).ToString("F2");
+                    }
+                    else if ((Tmoth - Lmoth) <= 30)
+                    {
+                        float Uer = Tmoth - Lmoth;
+                        txtMember.Text = "8.699";
+                        txtUserSd.Text = Uer.ToString();
+                        txtTongTien.Text = (float.Parse(txtMember.Text) * float.Parse(txtUserSd.Text) * 0.1).ToString("F2");
+                    }
+                    else
+                if ((Tmoth - Lmoth) < 0)
+                    {
+                        MessageBox.Show("Required to enter This month as greater than Last month");
+                        txtTmonth.Focus();
+                        txtLmoth.Focus();
+                    }
+                    else
+                    {
+                        float Uer = Tmoth - Lmoth;
+                        txtMember.Text = " 15.929";
+                        txtUserSd.Text = Uer.ToString();
+                        txtTongTien.Text = (float.Parse(txtMember.Text) * float.Parse(txtUserSd.Text) * 0.1).ToString("F2");
+                    }
+                }
+            }
+            else if (cbType.SelectedIndex == 1)
+            {
+                if (txtTmonth.Text == "" || txtLmoth.Text == "")
+                {
+                    MessageBox.Show("Enter Last moth and This moth in full");
+                }
+                else
+                {
+                    float Lmoth = float.Parse(txtLmoth.Text);
+                    float Tmoth = float.Parse(txtTmonth.Text);
+                    if ((Tmoth - Lmoth) > 0)
+                    {
+                        float Uer = Tmoth - Lmoth;
+                        txtMember.Text = " 9.955";
+                        txtUserSd.Text = Uer.ToString();
+                        txtTongTien.Text = (float.Parse(txtMember.Text) * float.Parse(txtUserSd.Text) * 0.1).ToString("F2");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Required to enter This month as greater than Last month");
+                    }    
+                }
+            }
+            else if (cbType.SelectedIndex == 2)
+            {
+                if (txtTmonth.Text == "" || txtLmoth.Text == "")
+                {
+                    MessageBox.Show("Enter Last moth and This moth in full");
+                }
+                else
+                {
+                    float Lmoth = float.Parse(txtLmoth.Text);
+                    float Tmoth = float.Parse(txtTmonth.Text);
+                    if ((Tmoth - Lmoth) > 0)
+                    {
+                        float Uer = Tmoth - Lmoth;
+                        txtMember.Text = " 11.615";
+                        txtUserSd.Text = Uer.ToString();
+                        txtTongTien.Text = (float.Parse(txtMember.Text) * float.Parse(txtUserSd.Text) * 0.1).ToString("F2");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Required to enter This month as greater than Last month");
+                    }
+                }
+            }
+            else if (cbType.SelectedIndex == 3)
+            {
+                if (txtTmonth.Text == "" || txtLmoth.Text == "")
+                {
+                    MessageBox.Show("Enter Last moth and This moth in full");
+                }
+                else
+                {
+                    float Lmoth = float.Parse(txtLmoth.Text);
+                    float Tmoth = float.Parse(txtTmonth.Text);
+                    if ((Tmoth - Lmoth) > 0)
+                    {
+                        float Uer = Tmoth - Lmoth;
+                        txtMember.Text = " 22.608";
+                        txtUserSd.Text = Uer.ToString();
+                        txtTongTien.Text = (float.Parse(txtMember.Text) * float.Parse(txtUserSd.Text) * 0.1).ToString("F2" );
+                    }
+                    else
+                    {
+                        MessageBox.Show("Required to enter This month as greater than Last month ");
+                    }
+                }
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            txtTongTien.Text = " ";
+            txtLmoth.Text = " ";
+            txtID.Text = " ";
+            txtMember.Text = " ";
+            txtUserSd.Text = " ";
+            txtName.Text = "  ";
+            txtPhone.Text = " ";
+            txtTmonth.Text = "";
+        }
+
+    }
+    }
+
